8f31fd480bc2216b2654f9ac049fb6de0c9abbc4
Probuse <nprobuse@gmail.com>
2025-04-26 13:38:34 +0300

    Enable user to CRUD Product

---
+++ product/forms.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, TextInput, NumberInput, Textarea
+
+from product.models import Product
+
+
+class TextArea:
+    pass
+
+
+class ProductModelForm(ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Product Name'}),
+            'description': Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Product Description',  'rows': 5,
+                'cols': 40,}),
+            'price': NumberInput({'class': 'form-control'}),
+        }
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if len(name) < 3:
+            raise ValidationError('Name must be 3 characters long or more')
+        return name
+
